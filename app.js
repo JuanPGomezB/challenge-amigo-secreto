eafed6befe697b48fbcc0704b6ed491484346886
JuanPGomezB <jugomezb@unal.edu.co>
2025-03-13 16:10:11 -0500

    Agregar amigo y Lista de amigos completado

--- app.js
+++ app.js
@@ -4,10 +4,30 @@ let amigos=[];
 
 function agregarAmigo(){
     let nuevoamigo = document.getElementById("amigo").value;
+    if (nuevoamigo === ""){
+        alert("Debes ingresar un nombre");
+        return;
+    }
+
     amigos.push(nuevoamigo);
     document.getElementById("amigo").value = "";
+    recorridoarray();
     // console.log(nuevoamigo);
     // console.log(amigos);
+}
+
+function recorridoarray(){
+    let lista = document.getElementById("listaAmigos");
+    // console.log(lista.innerHTML)
+
+    //vaciado de contenido: los Li exixtentes se quitan para ser reiniciados
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
 }
 
 function sortearAmigo(){
