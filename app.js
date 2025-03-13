5ccc66fc71c85d961a2f721bf6ad46b8bb192e0d
JuanPGomezB <jugomezb@unal.edu.co>
2025-03-13 15:18:51 -0500

    archivos y func. agregarAmigo

---
+++ app.js
@@ -0,0 +1,14 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos=[];
+
+function agregarAmigo(){
+    let nuevoamigo = document.getElementById("amigo").value;
+    amigos.push(nuevoamigo);
+    document.getElementById("amigo").value = "";
+    // console.log(nuevoamigo);
+    // console.log(amigos);
+}
+
+function sortearAmigo(){
+}
